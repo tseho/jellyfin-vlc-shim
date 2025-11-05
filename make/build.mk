4cc259d41570ab50cce49c02d7ee786e96bb24c1
Tseho <quentin.favrie@gmail.com>
2025-11-05 12:46:56 +0100

    Fix missing directory in release workflow

--- make/build.mk
+++ make/build.mk
@@ -3,6 +3,7 @@ build:
 	go build -o bin/jellyfin-vlc-shim cmd/jellyfin-vlc-shim/main.go
 
 bin/jellyfin-vlc-shim-linux-amd64:
+	@mkdir -p bin
 	docker buildx build --load --progress=plain --platform linux/amd64 --build-arg ARCH=amd64 -t jellyfin-vlc-shim:linux-amd64 -f Dockerfile .
 	docker rm jellyfin-vlc-shim-linux-amd64-container || true
 	docker create --name jellyfin-vlc-shim-linux-amd64-container jellyfin-vlc-shim:linux-amd64
@@ -10,6 +11,7 @@ bin/jellyfin-vlc-shim-linux-amd64:
 	docker rm jellyfin-vlc-shim-linux-amd64-container
 
 bin/jellyfin-vlc-shim-linux-arm64:
+	@mkdir -p bin
 	docker buildx build --load --progress=plain --platform linux/arm64 --build-arg ARCH=arm64 -t jellyfin-vlc-shim:linux-arm64 -f Dockerfile .
 	docker rm jellyfin-vlc-shim-linux-arm64-container || true
 	docker create --name jellyfin-vlc-shim-linux-arm64-container jellyfin-vlc-shim:linux-arm64
